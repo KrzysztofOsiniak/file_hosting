2e31826072354800fb814ec7baaebe9a91e849f2
KrzysztofOsiniak <krzysztofosiniak37@gmail.com>
2025-11-13 12:53:20 +0100

    save

--- frontend/src/repository.tsx
+++ frontend/src/repository.tsx
@@ -184,9 +184,9 @@ export default function Repository() {
         return
     }
 
-    async function handleDownload(id: number, e: React.MouseEvent<SVGSVGElement>) {
+    async function handleDownload(file: S3File, e: React.MouseEvent<SVGSVGElement>) {
         e.stopPropagation()
-        const res = await fetch(`/api/file/${id}`, {
+        const res = await fetch(`/api/file/${file.id}`, {
             method: 'GET'
         })
         if(res.status !== 200) {
@@ -194,17 +194,20 @@ export default function Repository() {
             setWarningPopup(true)
             return
         }
+
         const body = await res.json()
         const s3Res = await fetch(body.url)
-        const blob = await s3Res.blob()
-        const blobUrl = URL.createObjectURL(blob)
-        
-        const a = document.createElement('a')
-        a.href = blobUrl
-        a.download = ""
-        a.click()
-        
-        URL.revokeObjectURL(blobUrl)
+        // const fileHandle = await window.showSaveFilePicker()
+        // Save the file loading it to ram first.
+        // const blob = await s3Res.blob()
+        // const blobUrl = URL.createObjectURL(blob)
+        // const a = document.createElement('a')
+        // a.href = blobUrl
+        // const name = file.path.split('/').pop()
+        // a.download = typeof name === "undefined" ? "" : name
+        // a.click()
+        // a.remove()
+        // URL.revokeObjectURL(blobUrl)
     }
 
     async function handleFileNameChange(e: React.MouseEvent<SVGSVGElement>) {
@@ -372,7 +375,7 @@ export default function Repository() {
                     <div className={css.username}>{file.ownerUsername}</div>
                     <div className={css.size}>{getUnitSize(file.size)}{getUnit(file.size)}</div>
                     <div className={css.uploadDate}>{timeAgo(file.uploadDate)}</div>
-                    <svg onClick={e => handleDownload(file.id, e)} className={css.downloadIcon} viewBox="0 -960 960 960"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
+                    <svg onClick={e => handleDownload(file, e)} className={css.downloadIcon} viewBox="0 -960 960 960"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
                     <svg onClick={e => handleFileNameChange(e)} className={css.editIcon} viewBox="0 -960 960 960"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
                     <svg onClick={e => handleFileDelete(file.id, e)} className={css.deleteIcon} viewBox="0 -960 960 960"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
                 </div>)
